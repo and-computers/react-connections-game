aeb924852a111199d9f9918934d624fc8536d5d1
tosin <tosin.sonuyi@gmail.com>
2023-10-20 18:43:18 -0400

    better date countdown string rendering

--- src/components/CountdownToNextPuzzle/CountdownToNextPuzzle.js
+++ src/components/CountdownToNextPuzzle/CountdownToNextPuzzle.js
@@ -4,10 +4,40 @@ import { dateOfNextPuzzle } from "../../lib/time-utils";
 
 // Renderer callback with condition
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  //ChatGPT Assisted
   // Render a countdown
-  return (
-    <span>{`Next Puzzle Realeased in ${days} Days and ${hours} Hours`}</span>
-  );
+  if (completed) {
+    return (
+      <span className="font-[600]">
+        New Game Available! Refresh Your Browser.
+      </span>
+    );
+  }
+  const timeParts = [];
+
+  if (days > 0) {
+    timeParts.push(`${days} Day${days > 1 ? "s" : ""}`);
+  }
+  if (hours > 0) {
+    timeParts.push(`${hours} Hour${hours > 1 ? "s" : ""}`);
+  }
+  if (minutes > 0) {
+    timeParts.push(`${minutes} Minute${minutes > 1 ? "s" : ""}`);
+  }
+
+  // Create a sentence based on the time parts
+  let timeLeftString = "Next Puzzle Released in ";
+  if (timeParts.length > 2) {
+    timeLeftString += timeParts.slice(0, -1).join(", ");
+    timeLeftString += `, and ${timeParts.slice(-1)}`;
+  } else if (timeParts.length == 2) {
+    timeLeftString += `${timeParts[0]} and ${timeParts[1]}`;
+  } else {
+    timeLeftString += timeParts[0];
+  }
+  // Create a sentence based on the time parts
+
+  return <div>{timeLeftString}</div>;
 };
 
 function CountdownToNextPuzzle() {
