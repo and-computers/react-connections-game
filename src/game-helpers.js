98ffe7a617158be92e3cd8aefe25c00309319d3d
tosin <tosin.sonuyi@gmail.com>
2023-10-20 01:15:40 -0400

    emoji grid for sharing logic and sharing

--- src/game-helpers.js
+++ src/game-helpers.js
@@ -1,3 +1,5 @@
+import { UAParser } from "ua-parser-js";
+
 import {
   shuffle,
   chunk,
@@ -75,3 +77,117 @@ export function isGuessRepeated({ submittedGuesses, guessCandidate }) {
 
   return false;
 }
+
+// Share logic from here: https://github.com/cwackerfuss/react-wordle/blob/main/src/lib/share.ts
+const webShareApiDeviceTypes = ["mobile", "smarttv", "wearable"];
+const parser = new UAParser();
+const browser = parser.getBrowser();
+const device = parser.getDevice();
+
+const GAME_TITLE = "Black Connections";
+const solutionIndex = 1;
+
+export const shareStatus = (
+  gameData,
+  submittedGuesses,
+  handleShareToClipboard,
+  handleShareFailure
+) => {
+  const textToShare =
+    `${GAME_TITLE} #${solutionIndex}\n\n` +
+    generateEmojiGrid(gameData, submittedGuesses, getEmojiTiles());
+
+  const shareData = { text: textToShare };
+
+  let shareSuccess = false;
+
+  try {
+    if (attemptShare(shareData)) {
+      navigator.share(shareData);
+      shareSuccess = true;
+    }
+  } catch (error) {
+    shareSuccess = false;
+  }
+
+  try {
+    if (!shareSuccess) {
+      if (navigator.clipboard) {
+        navigator.clipboard
+          .writeText(textToShare)
+          .then(handleShareToClipboard)
+          .catch(handleShareFailure);
+      } else {
+        handleShareFailure();
+      }
+    }
+  } catch (error) {
+    handleShareFailure();
+  }
+};
+
+export const generateEmojiGrid = (
+  gameData,
+  submittedGuesses,
+  tiles = getEmojiTiles()
+) => {
+  const wordToDifficultyMap = {};
+
+  const numCategories = gameData.length;
+  const allWords = [];
+  for (let i = 0; i < numCategories; i++) {
+    allWords.push(gameData[i].words);
+
+    let difficulty = gameData[i].difficulty;
+    gameData[i].words.map((word) => (wordToDifficultyMap[word] = difficulty));
+  }
+
+  const allEmojiRowsArray = [];
+
+  for (let i = 0; i < submittedGuesses.length; i++) {
+    const submittedGuess = submittedGuesses[i];
+
+    let wordDifficultiesArray = submittedGuess.map(
+      (word) => wordToDifficultyMap[word]
+    );
+
+    const emojiRowForGuess = wordDifficultiesArray
+      .map((wordDifficulty) => {
+        switch (wordDifficulty) {
+          case 1:
+            return tiles[0];
+          case 2:
+            return tiles[1];
+          case 3:
+            return tiles[2];
+          case 4:
+            return tiles[3];
+        }
+      })
+      .join("");
+
+    allEmojiRowsArray.push(emojiRowForGuess);
+  }
+
+  return allEmojiRowsArray.join("\n");
+};
+
+const attemptShare = (shareData) => {
+  return (
+    // Deliberately exclude Firefox Mobile, because its Web Share API isn't working correctly
+    browser.name?.toUpperCase().indexOf("FIREFOX") === -1 &&
+    webShareApiDeviceTypes.indexOf(device.type ?? "") !== -1 &&
+    navigator.canShare &&
+    navigator.canShare(shareData) &&
+    navigator.share
+  );
+};
+
+const getEmojiTiles = () => {
+  let tiles = [];
+  tiles.push("🟩");
+  tiles.push("🟨");
+  tiles.push("🟪");
+  tiles.push("🟦");
+  return tiles;
+};
