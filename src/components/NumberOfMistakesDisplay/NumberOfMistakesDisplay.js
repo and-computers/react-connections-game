ec112534a5565e90e6389e5632ea7decc1f35b77
tosin <tosin.sonuyi@gmail.com>
2023-10-19 20:16:56 -0400

    add margin on mistakes remaining and remove unneeded div, increase font size

--- src/components/NumberOfMistakesDisplay/NumberOfMistakesDisplay.js
+++ src/components/NumberOfMistakesDisplay/NumberOfMistakesDisplay.js
@@ -6,9 +6,9 @@ function SingleMistakeDisplay({ isUsed }) {
   return (
     <div>
       {isUsed ? (
-        <CircleSlash className="h-4 w-4 stroke-neutral-400" />
+        <CircleSlash className="h-4 w-4 mt-1 stroke-neutral-400" />
       ) : (
-        <Circle className="h-4 w-4 fill-green-300 stroke-cyan-300" />
+        <Circle className="h-4 w-4 mt-1 fill-green-300 stroke-cyan-300" />
       )}
     </div>
   );
@@ -18,13 +18,11 @@ function NumberOfMistakesDisplay({ maxMistakes, numMistakesUsed }) {
   // array size of number of guess. [1, 2, 3, 4]
   const mistakeRange = range(maxMistakes);
   return (
-    <div>
-      <div className="flex flex-row gap-x-4">
-        <p className="text-xs">Mistakes Remaining: </p>
-        {mistakeRange.map((el) => (
-          <SingleMistakeDisplay key={el} isUsed={el < numMistakesUsed} />
-        ))}
-      </div>
+    <div className="flex flex-row gap-x-4">
+      <p className="text-base">Mistakes Remaining: </p>
+      {mistakeRange.map((el) => (
+        <SingleMistakeDisplay key={el} isUsed={el < numMistakesUsed} />
+      ))}
     </div>
   );
 }
